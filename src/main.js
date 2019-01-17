748d9563c8dbdb40dc264fc731db040bf0d7c00c
Andy Boughton <abought@gmail.com>
2019-01-17 12:27:16 -0500

    Add sentry + deployment instructions

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 // /* global $ */
 import Vue from 'vue';
 
+import * as Sentry from '@sentry/browser';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 import App from './App.vue';
+
+Sentry.init({
+    dsn: process.env.VUE_APP_SENTRY_DSN,
+    integrations: [new Sentry.Integrations.Vue({ Vue })],
+});
 
 new Vue({ render: h => h(App) })
     .$mount('#app');
